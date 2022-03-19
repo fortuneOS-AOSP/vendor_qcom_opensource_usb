42ca68542900729ad3eee8fe5744167544954ad4
Krishna Kurapati <quic_kriskura@quicinc.com>
2022-03-20 01:30:50 +0530

    USB: Use USB QDSS data over SW path on Neo
    Set the mode for the QDSS data path to go over the SW path instead
    of BAM for Neo.
    
    Change-Id: I91a1feb6a30d981fabe0f62f688b0f3d661998a9
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>

--- vendor_product.mk
+++ vendor_product.mk
@@ -21,7 +21,7 @@ else
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter lahaina taro parrot blair kalama pineapple monaco pitti niobe volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter lahaina taro parrot neo blair kalama pineapple monaco pitti niobe volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
