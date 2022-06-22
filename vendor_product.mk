cbf7399472fddccbf15555777ddedb54cc5c7dfc
Himanshu Agrawal <quic_c_hagraw@quicinc.com>
2022-06-22 11:45:12 +0530

    usb: Enable USB configurations for bengal
    Use software path for USB QDSS data transfer and
    Enable USBGadget HAL for bengal. Update the value
    of vendor.usb.controller being used based on targets.
    
    Change-Id: Icd7e1f27259663b69927e97480647fdf9197212e

--- vendor_product.mk
+++ vendor_product.mk
@@ -14,14 +14,14 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
-ifneq ($(filter blair monaco pitti,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal blair monaco pitti,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a600000.dwc3
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter lahaina taro parrot neo anorak blair kalama pineapple monaco pitti niobe volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal lahaina taro parrot neo anorak blair kalama pineapple monaco pitti niobe volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
@@ -59,7 +59,7 @@ else
   # USB Gadget HAL is enabled on newer targets and takes the place
   # of the init-based configfs rules for setting USB compositions
   #
-  ifneq ($(filter niobe taro kalama neo parrot anorak pineapple blair monaco pitti volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter bengal niobe taro kalama neo parrot anorak pineapple blair monaco pitti volcano anorak61,$(TARGET_BOARD_PLATFORM)),)
     PRODUCT_PROPERTY_OVERRIDES += vendor.usb.use_gadget_hal=1
     PRODUCT_PACKAGES += android.hardware.usb.gadget-service.qti
     PRODUCT_PACKAGES += usb_compositions.conf
